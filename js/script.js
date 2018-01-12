4b729f780d43b4f7f6e86bf7b0839af1fa86b51b
Michal Dokupil <michal@dokupil.pl>
2018-01-12 20:06:58 +0100

    first version of slider with bugs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,64 @@
 $(function(){
+  $('.options').on('click',function () {
+    if($(this).css("margin-left") !== "200px") {
+      $(this).animate({'margin-left': '+250'}, 300);
+       $('.btn').show(300);
+       
+    } else {
+      $(this).animate({'margin-left': '-250'}, 300);
+      $('.btn').hide(300);
+   } 
+  });
+  //interVal;
+  $('#left').click(function () {
+    moveLeft();
+  }); 
+  $('#right').click(function () {
+    moveRight();
+  }); 
 
+  var carouselList = $("#carousel ul");
+  var imagePosition = carouselList.find("li");
+        console.log(imagePosition);
+  var interVal = setInterval(changeSlide,2500);
+  var marginReset = carouselList.css({marginLeft:0});
+
+
+
+  function changeSlide(){
+    carouselList.animate({'marginLeft':'-800'}, 1000, moveFirstSlide);
+    interVal;
+  };
+  
+
+  function moveFirstSlide() {
+    moveImage();
+    carouselList.css({marginLeft:0});
+    interVal;
+  };
+
+  function moveLeft() {
+    if (imagePosition.lenght) {
+
+    }
+    stopInterval();
+    carouselList.stop().animate({'margin-left': '+=600'},500);;
+    firstItem.after(lastItem);
+  };
+
+  function moveRight() {
+     stopInterval();  
+    carouselList.stop().animate({'margin-left': '-=600'},500, moveImage);
+    setInterval(changeSlide(),2500);
+  };
+  
+  function stopInterval() {
+    clearInterval(interVal);
+  }
+
+  function moveImage() {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+  }
 });
